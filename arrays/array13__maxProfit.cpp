e7ee26ecdc6bda85e680895b1e12b09fdf9d2b56
Suryansh <suryanshgrover1999@gmail.com>
2022-09-12 21:12:42 +0530

    added another approach!

--- arrays/array13__maxProfit.cpp
+++ arrays/array13__maxProfit.cpp
@@ -6,89 +6,104 @@ using namespace std;
 class Solution
 {
 public:
-    int max_Profit(vector<int>& prices) {
-        int min_price = INT_MAX;
+    int maxProfit_i(vector<int> &prices) // O(n) time complexity and O(n) space taken!
+    {
+        vector<int> aux; // max price of stock on a certain day
+        /* Think of it as this way:
+        Assume today if I'd know on what day the stock is going to be sold at maximum price,
+        I'd be able to have maximum profit.
+        */
+
+        int max_so_far = prices[prices.size() - 1];
+        for (int i = prices.size() - 1; i >= 0; i--)
+        {
+            if (max_so_far < prices[i])
+            {
+                aux.push_back(prices[i]);
+                max_so_far = prices[i];
+            }
+            else
+            {
+                aux.push_back(max_so_far);
+            }
+        }
+        reverse(aux.begin(), aux.end());
+
+        int max_profit = 0;
+
+        for (int i = 0; i < prices.size(); i++)
+        {
+            if (aux[i]-prices[i] > max_profit)
+            {
+                max_profit = aux[i]-prices[i];
+            }
+        }       
+        // displaying the aux array
+        // for (int i = 0; i < aux.size(); i++)
+        // {
+        //     cout << aux[i] << " ";
+        // }
+        // cout << endl;
+
+        return max_profit;
+    }
+
+    int maxProfit_ii(vector<int> &prices) // O(n) time complexity!
+    {   
+        /*
+        Think of this approach as this way:
+        Try to sell the stock on each day and think over what was the day before today where you could've
+        bought it for the lowest price.
+        */
+        int min_price = prices[0];
         int profit = 0;
         for (int i = 0; i < prices.size(); i++)
         {
-            min_price = min(prices[i], min_price);
-            profit = max(profit, prices[i]-min_price);
+            min_price = min(min_price, prices[i]);
+            profit = max(profit, prices[i] - min_price);
         }
-        return profit;        
+        return profit;
     }
 
-    int maxProfit(vector<int>& prices) {
-        
+    int maxProfit_iii(vector<int> &prices)
+    {
+
         int cheapest = 0;
         int exp = 1;
         int profit = 0;
-        
-        for (int i=0; i<prices.size(); i++)
+
+        for (int i = 0; i < prices.size(); i++)
         {
             // had to buy on the day on which price is lowest
             if (prices[i] < prices[cheapest])
             {
-                cheapest=i;
-                exp=i+1; // it must be kept on increasing so that day[sell] > day[buy] always
+                cheapest = i;
+                exp = i + 1; // it must be kept on increasing so that day[sell] > day[buy] always
             }
-            for(int j=i+1; j<prices.size(); j++)
+            for (int j = i + 1; j < prices.size(); j++)
             {
                 // had to sell on the day on which the price is highest!
-                if (prices[j] > prices[exp] )
+                if (prices[j] > prices[exp])
                 {
-                    exp=j;
+                    exp = j;
                 }
-                if (prices[exp]-prices[cheapest] > profit)
-                {   
-                   profit= prices[exp]-prices[cheapest];
+                if (prices[exp] - prices[cheapest] > profit)
+                {
+                    profit = prices[exp] - prices[cheapest];
                 }
             }
         }
         return profit;
     }
-
-    int max_profit(vector<int> &prices) // failed for some cases miserably!
-    {
-        int cheapest = 0;
-        // finding the day when the stock pricers is lowest
-        for (int i = 1; i < prices.size(); i++)
-        {
-            if (prices[i] < prices[cheapest])
-            {
-                cheapest = i;
-            }
-        }
-        // checking if today is that so only
-        if (cheapest == prices.size() - 1)
-        {
-            return 0;
-        }
-        else
-        {
-            int exp = cheapest;
-            // finding the day when the price is expensive
-            for (int i = cheapest + 1; i < prices.size(); i++)
-            {
-                if (prices[i] > prices[exp])
-                {
-                    exp = i;
-                }
-            }
-            if (prices[cheapest] < prices[exp])
-            {
-                return prices[exp] - prices[cheapest];
-            }
-        }
-        return 0;
-    }
 };
 
 int main()
-{   
+{
     // vector<int> prices = {2,4,1};
-    vector<int> prices = {7,6,4,3,1};
+    // vector<int> prices = {7, 6, 4, 3, 1};
+    vector<int> prices = {300, 100, 400, 800, 700, 200, 500};
     Solution sol;
-    cout << "max profit: " << sol.maxProfit(prices) << endl;
-    cout << "max profit: " << sol.max_Profit(prices) << endl;
+    cout << "max profit: " << sol.maxProfit_i(prices) << endl;
+    cout << "max profit: " << sol.maxProfit_ii(prices) << endl;
     return 0;
 }
